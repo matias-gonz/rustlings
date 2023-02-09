6f1a6558736ada9daea7176d0558686aee8ee288
matias-gonz <maigonzalez@fi.uba.ar>
2023-02-09 15:54:30 -0300

    Solve conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,16 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let person = s.split(",").collect::<Vec<&str>>();
+        if person.len() != 2 || person[0].len() == 0{
+            return Person::default();
+        }
+        match person[1].parse::<usize>() {
+            Ok(age) => Person{name: person[0].to_string(), age},
+            _ => Person::default()
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0{
+            return Err(ParsePersonError::Empty);
+        }
+        let person = s.split(",").collect::<Vec<&str>>();
+        match person.len() {
+            2 => {},
+            _ => return Err(ParsePersonError::BadLen)
+        }
+        if person[0].len() == 0{
+            return Err(ParsePersonError::NoName);
+        }
+
+        match person[1].parse::<usize>() {
+            Ok(age) => Ok(Person{name: person[0].to_string(), age}),
+            Err(err) => Err(ParsePersonError::ParseInt(err))
+        }
     }
 }
 
